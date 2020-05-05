a808e4b1f343763a5394b3aff18a31b56bd14781
Carl Dunkelberger <carl.dunkelberger@docker.com>
2020-05-05 17:00:14 -0400

    adding container inspect

--- collect-interlock-logs.sh
+++ collect-interlock-logs.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -x
+
 LOGDIR="/tmp/interlock-logs$(hostname)-$(date -u +'%s')"
 mkdir $LOGDIR
 
 for i in $(docker ps -a -f name=interlock -q); do
     docker container logs $i &> $LOGDIR/$i.log
 done
+
+for i in $(docker ps -a -f name=interlock -q); do
+    docker container inspect $i &> $LOGDIR/$i-inspect.json
+done
+
 
 for i in $(docker service ls -f name=ucp-interlock -q); do docker service inspect $i > $LOGDIR/$i-inspect.json; done
